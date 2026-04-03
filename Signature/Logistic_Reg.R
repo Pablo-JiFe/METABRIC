8c503750ea5a3bd968d6e0d9612e73c262d6ab53
Mar Hernández <marianahdez1820@gmail.com>
2026-04-02 20:17:12 -0600

    Refactor Logistic Regression script with comments
    Added comments for clarity and improved code structure.

--- Signature/Logistic_Reg.R
+++ Signature/Logistic_Reg.R
@@ -19,44 +19,47 @@ data_split <- initial_split(late_genes.patients, strata = SURVIVAL)
 train_data <- training(data_split)
 test_data  <- testing(data_split)
 
-#
-
+# Create recipe that defines the model formula
 late_genes.rec <- 
-  recipe(SURVIVAL ~ ., data = train_data)
+  recipe(SURVIVAL ~ ., data = train_data) # Survival is the outcome and all variables are predictors
 
+# Set model (glm for logistic regression)
 late_genes.mod <- 
   logistic_reg() %>% 
   set_engine("glm")
 
-
+# Workflow with the recipe and model
 late_genes.wf <- 
   workflow() %>% 
   add_model(late_genes.mod) %>% 
-  add_recipe(late_genes.rec) 
+  add_recipe(late_genes.rec)
 
+# Fit logistic regression model with training data
 late_genes.fit <- 
   late_genes.wf %>% 
   fit(data = train_data)
 
+# Extract fitted model
 late_genes.fit %>% 
   extract_fit_parsnip() %>% 
   tidy()
 
-
+# Predictions on the test set
 late_genes.aug <- 
   augment(late_genes.fit, test_data)
 
+# ROC 
 late_genes.aug %>% 
   roc_curve(truth = SURVIVAL, .pred_0) %>% 
   autoplot()
-
 
 lr_auc <- late_genes.aug %>%
   roc_curve(SURVIVAL, .pred_0) %>%   
   mutate(model = "Logistic Regression")
 
+# Plot
 autoplot(lr_auc)
 
-
+# AUC
 late_genes.aug %>% 
   roc_auc(truth = SURVIVAL, .pred_0)
