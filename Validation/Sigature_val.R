d0bf983b01088ebea34cb917d207996c238a5c5c
pablojimenezferia <pablojimenez@lasallistas.org.mx>
2026-04-10 13:59:58 -0600

    Refactor signature validation for GSE 2043
    Updated the script for signature validation on GSE 2043, including loading data, preprocessing metadata, and validation steps.

--- Validation/Sigature_val.R
+++ Validation/Sigature_val.R
@@ -1,4 +1,4 @@
-# Script to perform normalization, annotation and batch correction for PCA and cluster analysis
+# This script is to perform signature validation on GSE 2043
 
 library(oligo)
 library(GEOquery)
@@ -10,10 +10,9 @@ library(readr)
 
 
 
-# 1.- Load data and metadata ----------------------------------------------
+# 1.- Load data and metadata_gse ----------------------------------------------
 
 # 1.1 Get supplementary files
-# The use of supp files (.cel) is for future analysis of multiple data bases
 
 # getGEOSuppFiles("GSE2034", baseDir = "D:/tcga")
 
@@ -30,13 +29,13 @@ pre_raw_data <- read.celfiles(cel_files)
 
 raw_data <- pre_raw_data
 
-# 1.2 Metadata
+# 1.2 metadata_gse
 
 pre_metadata <- read.delim("C:/R/METABRIC/Validation/GSE2034/Metadatos/GSE2034_metadata", sep = ",")
-metadata <- pre_metadata
+metadata_gse <- pre_metadata
 
 #> pData of raw_data initially only contains the ID for the counts and an index
-#> meanwhile metadata contains the full metadata but the IDs differ from the count data IDs
+#> meanwhile metadata_gse contains the full metadata but the IDs differ from the count data IDs
 
 # Object that specifies which GSE is in use
 
@@ -44,15 +43,15 @@ gse_obj <- "GSE2034"
 
 
 
-# 3.- Preprocessing metadata --------------------------------------------------
+# 3.- Preprocessing metadata_gse --------------------------------------------------
 
-#3.1 Clean metadata
+#3.1 Clean metadata_gse
 
-metadata <- metadata %>%
+metadata_gse <- metadata_gse %>%
   mutate(
-    SURVIVAL = ifelse(relapse..1.True. == 1, 1, 0),
+    SURVIVAL = ifelse(relapse..1.True. == 1, 1, 0), # Object to evaluate event
     SURVIVAL = as.numeric(SURVIVAL),
-    SURVIVAL_MON = time.to.relapse.or.last.follow.up..months.,
+    SURVIVAL_MON = time.to.relapse.or.last.follow.up..months., # Object to evaluate time to event
     SURVIVAL_MON = as.numeric(SURVIVAL_MON),
     id = GEO.asscession.number
     )
@@ -60,32 +59,35 @@ metadata <- metadata %>%
 # 3.2 Object with the names of each file
 
 id <- sampleNames(raw_data)
-sample_names <- gsub("\\..*", "", id)
-# 3.2.2 Add the object to the phenotipic data
+sample_names <- gsub("\\..*", "", id) # Raking out the .CEL.gz
+
+# 3.2.2 Add the object with the general names to the phenotipic data
 
 pData(raw_data)$id <- sample_names
 
-# 3.4 Join metadata 
+# 3.4 Join metadata_gse 
 
 pData(raw_data) <- 
   pData(raw_data) %>% 
-  rownames_to_column("file_name") %>% 
-  full_join(metadata, by = "id", keep = FALSE) %>% 
-  mutate(comp_file_name = file_name) %>% 
-  column_to_rownames("file_name") %>% 
+  rownames_to_column("file_name") %>% # To have the full names
+  full_join(metadata_gse, by = "id", keep = FALSE) %>% # Join by names without .CEL.gz
+  mutate(comp_file_name = file_name) %>% # Create new column to then add to rownames
+  column_to_rownames("comp_file_name") %>% 
   mutate(file_name = comp_file_name,
          comp_file_name = NULL)
   
-metadata <- pData(raw_data)
+metadata_gse <- pData(raw_data) # Make shure they have the same data this so that metadata also has the identifiers with .CEL.gz to match with the counts
 
 
 # 4.- Preprocess data -----------------------------------------------------
 
 
 # Normalize
-norm_data <- rma(raw_data)
+
+norm_data <- rma(raw_data) 
 
 # Boxplot after normalization
+
 boxplot(exprs(norm_data), 
         las = 2, 
         main = paste0("RMA normalized - ", gse_obj))
@@ -97,9 +99,11 @@ expr_matrix <- exprs(norm_data)
 # 5.- Probe ID to symbol --------------------------------------------------
 
 # 5.1 Get mapping to change probe IDs to gene names
+
 library(hgu133a.db)
+
 probe_gene <- AnnotationDbi::select(
-  hgu133a.db,
+  hgu133a.db, # The probe identifiers for thius affymetrix
   keys = rownames(expr_matrix),
   columns = "SYMBOL",
   keytype = "PROBEID"
@@ -113,67 +117,67 @@ gene_expres_matrix <-
   expr_matrix %>%
   as.data.frame() %>%
   rownames_to_column("PROBEID") %>%
-  inner_join(probe_gene, by = "PROBEID") %>%  # Join with your mapping object
+  inner_join(probe_gene, by = "PROBEID") %>%  # Join with the mapping object
   filter(!is.na(SYMBOL) & SYMBOL != "") %>%   # Remove NAs and empty symbols
-  
-  # 5.2.2 Calculate variance for each probe across all samples
-  
-  mutate(variance = apply(dplyr::select(., -PROBEID, -SYMBOL), 1, var)) %>%
-  
-  # 5.2.3 Keep only the probe with the highest variance per Gene Symbol
-  
-  group_by(SYMBOL) %>%
-  slice_max(order_by = variance, n = 1, with_ties = FALSE) %>% 
+  mutate(variance = apply(dplyr::select(., -PROBEID, -SYMBOL), 1, var)) %>%   # 5.2.2 Calculate variance for each probe across all samples
+  group_by(SYMBOL) %>% 
+  slice_max(order_by = variance, n = 1, with_ties = FALSE) %>% # 5.2.3 Keep only the probe with the highest variance per Gene Symbol
   ungroup() %>%
-  
-  # 5.2.4 Remove columns, add symbol to rownames and reformat to matrix
-  
-  dplyr::select(-PROBEID, -variance) %>%
+  dplyr::select(-PROBEID, -variance) %>%  # 5.2.4 Remove columns, add symbol to rownames and reformat to matrix
   column_to_rownames("SYMBOL") %>%
   as.matrix()
 
 
-# 1. Transpose so genes are rows for scaling
-# 2. scale() works on columns, so we scale, then transpose back 
-# so patients are rows for the prediction
-gse2034_scaled <- t(scale(t(gene_expres_matrix))) 
+# 5.3 Transpose so genes are rows for scaling
+# 5.3.2 scale() works on columns, so we tansposeso patients are rows for the prediction
 
-# Convert to a data frame so you can add clinical columns
-final_df <- as.data.frame(t(gse2034_scaled)) 
+gse2034_scaled <- scale(t(gene_expres_matrix)) 
 
-metadata <- metadata %>% 
+# 5.3.3 Convert to a data frame so to add clinical columns
+final_df <- as.data.frame(gse2034_scaled)
+
+# 5.4 Keep only ER+ patients
+
+metadata_gse <- metadata_gse %>% 
   filter(ER.Status == "ER+")
 
-final_df <- final_df[rownames(final_df) %in% metadata$file_name,] %>% 
+# 5.5 Keep oly patients that are found on the counts
+
+final_df <- final_df[rownames(final_df) %in% metadata_gse$file_name,] %>% 
   rownames_to_column("file_name")
 
 
 library(survival)
-# Join with the metadata you cleaned earlier
+# Join with the metadata_gse that was cleaned earlier
 final_df <- final_df %>%
-  left_join(metadata, by = "file_name") %>%
+  left_join(metadata_gse, by = "file_name") %>%
   mutate(
-    # Create the Survival Object inside the dataframe
-    surv_obj = Surv(time = SURVIVAL_MON, event = SURVIVAL)
+    surv_obj = Surv(time = SURVIVAL_MON, event = SURVIVAL) # Create the Survival Object inside the dataframe
   ) %>% 
   column_to_rownames("file_name")
 
 
-# Find genes present in both
-common_genes <- intersect(boruta_signature, colnames(final_df))
+# Find genes present in both data sets
+common_genes_meta.gse2043 <- intersect(boruta_signature, colnames(final_df))
 
-# Check how many you lost
-print(paste("Original:", length(boruta_signature), "Common:", length(common_genes)))
+# Check how many are lost
+print(paste("Original:", length(boruta_signature), "Common:", length(common_genes_meta.gse2043)))
 
 # If there are gene missing, run the lin reg with common_genes as the gene list
 
+##################################################################
 
-# This is the "testing" phase
+# 6.- Validation ----------------------------------------------------------
+
+
+# This is the predict phase
+
 gse2034_results <- predict(final_fit, new_data = final_df) %>%
   bind_cols(final_df)
 
 
-# This is where you get your "P-value" for the test
+# To get the p value
+
 validation_test <- coxph(Surv(SURVIVAL_MON, SURVIVAL) ~ .pred_time, data = gse2034_results)
 
 summary(validation_test)
@@ -181,14 +185,17 @@ summary(validation_test)
 
 library(survminer)
 
-# Create risk groups based on the median of your predictions
+# Create risk groups based on the median of the predictions
+
 gse2034_results <- gse2034_results %>%
   mutate(risk_group = ifelse(.pred_time < median(.pred_time), "High Risk", "Low Risk"))
 
 # Fit the KM curve
+
 km_fit <- survfit(Surv(SURVIVAL_MON, SURVIVAL) ~ risk_group, data = gse2034_results)
 
 # Plot
+
 ggsurvplot(km_fit, 
            data = gse2034_results, 
            pval = TRUE, 
@@ -201,12 +208,12 @@ gse2034_results <- gse2034_results %>%
   mutate(pred_z = scale(.pred_time))
 
 # Run Cox again
+
 summary(coxph(Surv(SURVIVAL_MON, SURVIVAL) ~ pred_z, data = gse2034_results))
 
 library(timeROC)
 
-# Assuming gse2034_results has: 
-# SURVIVAL_MON (time), SURVIVAL (event), and pred_z (your score)
+# Area under the curve per time
 
 res_auc <- timeROC(T = gse2034_results$SURVIVAL_MON,
                    delta = gse2034_results$SURVIVAL,
@@ -216,4 +223,5 @@ res_auc <- timeROC(T = gse2034_results$SURVIVAL_MON,
                    iid = TRUE)
 
 # View the AUC values
+
 print(res_auc$AUC)
