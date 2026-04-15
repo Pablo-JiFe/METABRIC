27a1839aff09bb35e24414af3c4f678205ec030c
pablojimenezferia <pablojimenez@lasallistas.org.mx>
2026-04-15 10:20:44 -0600

    Refactor GSE96058 data preprocessing script
    Updated the script to preprocess metadata and counts data for GSE96058, including changes to variable names and data processing steps.

--- Validation/Signature_val_GSE96058.R
+++ Validation/Signature_val_GSE96058.R
@@ -1,4 +1,4 @@
-#> In this script we obtain and preprocess the metadata and the counts data
+#> In this script we obtain and preprocess the metadata.gse96058 and the counts data
 
 library(GEOquery)
 library(tidyverse)
@@ -12,9 +12,9 @@ library(tidyverse)
 # 1.1.2 Read the specific expression file (SCAN-B typically provides a large .txt or .csv)
 
 raw <- read.csv("D:/GSE96058/GSE96058_gene_expression_3273_samples_and_136_replicates_transformed.csv.gz", 
-                          row.names = 1, check.names = FALSE)
-counts_data <- raw
-# 1.2 Download metadata
+                row.names = 1, check.names = FALSE)
+counts_data.gse96058 <- raw
+# 1.2 Download metadata.gse96058
 
 gse <- getGEO("GSE96058", GSEMatrix = TRUE)
 
@@ -24,12 +24,12 @@ pheno <- pData(gse[[1]])
 
 
 
-# 2.- Preprocess metadata -------------------------------------------------
+# 2.- Preprocess metadata.gse96058 -------------------------------------------------
 
 pheno$characteristics_ch1.3 <- gsub("\\D", "", pheno$characteristics_ch1.3)
 
 
-metadata <-
+metadata.gse96058 <-
   pheno %>%
   mutate(
     tissue = source_name_ch1,
@@ -53,7 +53,7 @@ metadata <-
     ki67_pred_sgc = as.numeric(`ki67 prediction sgc:ch1`),
     nhg_pred_mgc = as.numeric(`nhg prediction mgc:ch1`),
     pam50 = as.factor(`pam50 subtype:ch1`),
-    os_months = as.numeric(`overall survival days:ch1`),
+    os_months = as.numeric(`overall survival days:ch1`) / 30.4166667,
     os_status = as.numeric(`overall survival event:ch1`),
     endocrine_tx = as.numeric(`endocrine treated:ch1`),
     chemo_tx = as.numeric(`chemo treated:ch1`)
@@ -114,32 +114,177 @@ metadata <-
     )
   )
 
-counts_data[1:5,1:5]
+counts_data.gse96058[1:5,1:5]
 
-  
-metadata_her2_low <- 
-  metadata %>% 
-  filter(her2_status == 0)
+
+metadata.gse96058_er_pos <-
+  metadata.gse96058 %>% 
+  filter(er_pred_sgc == 1) %>% 
+  rownames_to_column("id") %>% 
+  mutate(SURVIVAL = os_status,
+         SURVIVAL_MON = os_months,
+         id = NULL) 
 
 # 3.- Preprocess data -----------------------------------------------------
 
-# 3.1 Match it with metadata
+# 3.1 Match it with metadata.gse96058
 
 # 3.1.1 Identify patients in both sets
 
-common_samples <- intersect(colnames(counts_data), metadata$title)
+common_samples <- intersect(colnames(counts_data.gse96058), metadata.gse96058_er_pos$title)
 
-common_samples_her2_low <- intersect(colnames(counts_data), metadata_her2_low$title)
+# 3.1.2 Keep the patients in counts data that also have metadata.gse96058
 
-# 3.1.2 Keep the patients in counts data that also have metadata
+counts_data.gse96058_erpos <- counts_data.gse96058[, common_samples]
 
-counts_data <- counts_data[, common_samples]
+counts_data.gse96058_erpos <- scale(t(counts_data.gse96058_erpos))
 
+final_df <- 
+  counts_data.gse96058_erpos %>% 
+  as.data.frame() %>% 
+  rownames_to_column("title") %>% 
+  left_join(metadata.gse96058_er_pos, by = "title") %>% 
+  mutate(surv_obj = Surv(time = SURVIVAL_MON, event = SURVIVAL, type = "right")) %>% 
+  dplyr::select(all_of(late_death.genes),
+                SURVIVAL,
+                SURVIVAL_MON,
+                title,
+                surv_obj) %>% 
+  column_to_rownames("title")
 
-counts_her2_low <- counts_data[, common_samples_her2_low]
 
 #> //Dictionary//################################################
 #> 
-#> counts_data <-  Gene counts of the patients with available metadata
+#> counts_data.gse96058 <-  Gene counts of the patients with available metadata.gse96058
 #> 
-#> metadata_her2_low <- Processed metadata
+#> metadata.gse96058_her2_low <- Processed metadata.gse96058
+
+
+# Find genes present in both data sets
+common_genes_meta.gse96058 <- intersect(boruta_signature, colnames(counts_data.gse96058_erpos))
+
+
+# Check how many are lost
+print(paste("Original:", length(boruta_signature), "Common:", length(common_genes_meta.gse96058)))
+
+counts_data.gse96058_erpos <- counts_data.gse96058_erpos[ , common_genes_meta.gse96058]
+
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+
+# 6.- Validation ----------------------------------------------------------
+
+
+# This is the predict phase
+
+gse96058_results <- predict(final_fit, new_data = final_df, type = "linear_pred") %>%
+  bind_cols(final_df)
+
+
+# To get the p value
+
+validation_test <- coxph(Surv(SURVIVAL_MON, SURVIVAL) ~ .pred_linear_pred, data = gse96058_results)
+
+summary(validation_test)
+
+
+library(survminer)
+
+# Create risk groups based on the median of the predictions
+
+gse96058_results <- gse96058_results %>%
+  mutate(risk_group = as.factor(ifelse(.pred_linear_pred < median(.pred_linear_pred), "High Risk", "Low Risk")))
+
+
+# Fit the KM curve
+
+km_fit <- survfit(Surv(SURVIVAL_MON, SURVIVAL) ~ risk_group, data = gse96058_results)
+
+# Plot
+
+ggsurvplot(km_fit, 
+           data = gse96058_results, 
+           pval = TRUE, 
+           risk.table = TRUE,
+           title = "Validation in GSE2034 (Untreated Cohort)",
+           palette = c("#E41A1C", "#377EB8"))
+
+
+gse96058_results <- gse96058_results %>%
+  mutate(pred_z = scale(.pred_linear_pred))
+
+# Run Cox again
+gse96058_results$risk_group <- relevel(gse96058_results$risk_group, ref = "Low Risk")
+
+summary_gse96058 <- summary(coxph(Surv(SURVIVAL_MON, SURVIVAL) ~ risk_group, data = gse96058_results))
+
+library(timeROC)
+
+# Area under the curve per time
+
+res_auc <- timeROC(T = gse96058_results$SURVIVAL_MON,
+                   delta = gse96058_results$SURVIVAL,
+                   marker = -gse96058_results$pred_z,
+                   cause = 1, # The event code
+                   times = c(36, 60, 80), # 3, 5, and 10 years
+                   iid = TRUE)
+
+# View the AUC values
+
+res_auc_gse96058 <- res_auc$AUC %>% 
+  as.data.frame()
+
+text_validation.gse96058 <- paste("Esta firma en GSE96058 consiguio un HR de ",
+      round(summary_gse96058$coefficients[2], 3),
+      " (IC 95% de ",
+      round(summary_gse96058$conf.int[3], 3),
+      " - ",
+      round(summary_gse96058$conf.int[4], 3),
+      ", pval de ",
+      summary_gse96058$coefficients[5],
+      ", C score de ",
+      round(summary_gse96058$concordance[1], 2),
+      ", área bajo la curva a los 3 años de ",
+      round(res_auc_gse96058[1,1], 3),
+      ", a los 5 años de ",
+      round(res_auc_gse96058[2,1], 3),
+      ", y a los 6 años de",
+      round(res_auc_gse96058[3,1], 3)
+)
+
+cat(text_signature, text_validation.gse96058, sep = ". ")
+
+
+# Cox multivariado con clinica
+
+late_genes.patients_gse96058.cox <- 
+  final_df %>% 
+  as.data.frame() %>% 
+  rownames_to_column("title") %>% 
+  left_join(metadata.gse96058_er_pos, by = "title", suffix = c("", ".y")) %>%
+  dplyr::select(-ends_with(".y")) %>% 
+  column_to_rownames("title") %>% 
+  mutate(HER2 = her2_pred_sgc, 
+         LYMPH = lymph_group,
+         PAM50 = pam50,
+         AGE = as.numeric(age),
+         KI67 = ki67_pred_sgc,
+         SCORE = gse96058_results$.pred_linear_pred * - 1
+  ) %>% 
+  dplyr::select(all_of(late_death.genes),
+                surv_obj,
+                LYMPH,
+                PAM50,
+                AGE,
+                HER2,
+                KI67,
+                SCORE
+  ) %>% 
+  na.omit()
+
+coxph(surv_obj ~ PAM50 + KI67 + HER2 + AGE + LYMPH + SCORE, 
+      data = late_genes.patients_gse96058.cox)
